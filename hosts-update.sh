4f78c6ae12e9d38bc47bf639381cfb213360eb50
Deni (s55db) <b4d@sablun.org>
2024-05-31 10:24:03 +0200

    Update hosts-update.sh
    A spash of colors, disabling non-working blacklists.

--- hosts-update.sh
+++ hosts-update.sh
@@ -3,9 +3,20 @@
 DIR=/var/unbound
 FILENAME=ads.conf
 
-echo -e "Backing up old generated file..."
+DATE=$(date '+%Y-%m-%d')
+
+Color_Off='\033[0m'       # Text Reset
+Green='\033[0;32m'        # Green
+
+echo -e "${Green} *****************************${Color_Off}"
+echo -e "${Green} *${Color_Off} Unbound blocking script ${Green} *${Color_Off}"
+echo -e "${Green} *****************************${Color_Off}"
+
+
+echo " "
+echo -e "${Green} -- Backing up old generated file...${Color_Off}"
 #rm -v $DIR/$FILENAME-old
-cp -vbu $DIR/$FILENAME $DIR/$FILENAME-old
+cp -vbu $DIR/$FILENAME $DIR/$FILENAME-$DATE
 echo
 
 #echo -e "Removing old source file..."
@@ -14,22 +25,23 @@ echo
 
 mkdir -p $DIR/lists
 
-echo -e "Downloading new source file..."
+echo -e "${Green} -- Downloading new source file...${Color_Off}"
 wget -c -N -O $DIR/lists/stevenblack  https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 #wget -c -N -O $DIR/lists/malwaredom   https://mirror1.malwaredomains.com/files/justdomains
-wget -c -N -O $DIR/lists/cameleon     http://sysctl.org/cameleon/hosts
-wget -c -N -O $DIR/lists/zeustracker  https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist
-wget -c -N -O $DIR/lists/discontrack  https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt
-wget -c -N -O $DIR/lists/disconad     https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt
-wget -c -N -O $DIR/lists/hostsfile    https://hosts-file.net/ad_servers.txt
+#wget -c -N -O $DIR/lists/cameleon     http://sysctl.org/cameleon/hosts
+#wget -c -N -O $DIR/lists/zeustracker  https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist
+#wget -c -N -O $DIR/lists/discontrack  https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt
+#wget -c -N -O $DIR/lists/disconad     https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt
+#wget -c -N -O $DIR/lists/hostsfile    https://hosts-file.net/ad_servers.txt
 echo
 
-echo -e "Removing old generated file..."
+echo -e "${Green} -- Removing old generated file...${Color_Off}"
 rm -v $DIR/$FILENAME
 echo
 
-echo -e "Generating new file from the downloaded source..."
-echo -e "This will take some time..."
+echo -e "${Green} -- Generating new file from the downloaded source...${Color_Off}"
+echo -e "This will take some time...${Color_Off}"
+echo " "
 
 CAMELEON="$(cat lists/cameleon | grep -v 'localhost' | sed 's/\t//g' | sed 's/127.0.0.1/0.0.0.0/g' | grep '^0\.0\.0\.0')"
 HOSTSFILE="$(cat lists/hostsfile | grep -v 'localhost' | grep -v '^#' | sed 's/\t/ /g' | sed -e "s/\r//g" | sed 's/127.0.0.1/0.0.0.0/g' | grep '^0\.0\.0\.0')"
@@ -46,8 +58,9 @@ echo "$CLEAN" | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A
 
 #diff $DIR/$FILENAME $DIR/$FILENAME-old
 
-echo -e "Total number of blocked hosts:"
+echo -e "${Green} -- Total number of blocked hosts:${Color_Off}"
 echo "$CLEAN" | wc -l
+echo " "
 
-echo -e "Reloading unbound service configuration..."
+echo -e "${Green} -- Reloading unbound service configuration...${Color_Off}"
 service unbound reload
